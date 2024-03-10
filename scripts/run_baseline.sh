5f462123de99ef9410907f358a7c5d3b1d000464
LiXiaotao <40666875+Passer-montanus@users.noreply.github.com>
2024-03-10 23:16:04 +0800

    Update run_ausim.sh

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,6 +1,6 @@
-sudo docker rm -f baseline
+sudo docker rm -f ausim
 
-sudo docker run -it --name baseline --network=host \
+sudo docker run -it --name ausim --network=host \
     --gpus all \
     --privileged \
     --device=/dev/ttyUSB0 \
@@ -8,5 +8,5 @@ sudo docker run -it --name baseline --network=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -v $HOME/Desktop/shared:/shared \
-    jieyitsinghuawx/icra2024-sim2real-axs-baseline:v1.0.0 \
+    littledt/icra2024-sim2real-axs-ausim:v1.1 \
     /bin/bash
